1446c299f30a72d0e5be498045a7c2a72313fb12
Isaac Ndarwa <isaac.ndarwa@copiakenya.com>
2023-04-07 23:23:39 +0300

    Implemented the Create User API

---
+++ app/user/urls.py
@@ -0,0 +1,13 @@
+"""
+URL Mappings for the User API.
+"""
+from django.urls import path
+
+from user import views
+
+# Used for the reverse mapping defined in test_user_api
+app_name = 'user'
+
+urlpatterns = [
+    path('create/', views.CreateUserView.as_view(), name='create')
+]
